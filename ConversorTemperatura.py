05bcc9b179417a1c838e9123cdfc950ce960b20f
Lucas <landim@fedoraproject.org>
2017-09-07 00:26:25 -0300

    test

--- ConversorTemperatura.py
+++ ConversorTemperatura.py
@@ -3,3 +3,4 @@ _F_ = input("Digite uma temperatura em Fahrenheit: ")
 _C_ = (float(_F_) - 32) * 5 / 9
 
 print("A temperatura em celsius eh: ",_C_)
+
--- ex/l1_payment.py
+++ ex/l1_payment.py
@@ -1,7 +1,7 @@
 nome = input("Digite o nome do cliente: ")
 venc = input("Digite o dia de vencimento: ")
-mes = input("Digite o mês de vencimento: ")
+mes = input("Digite o mes de vencimento: ")
 fatura = input("Digite o valor da fatura: ")
 
-print("Olá,",nome)
-print("A sua fatura com vencimento em",venc,"de",mes,"no valor de R$",fatura,"está fechada.")
+print("Ola,",nome)
+print("A sua fatura com vencimento em",venc,"de",mes,"no valor de R$",fatura,"esta fechada.")
